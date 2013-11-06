eb1b22c82fdabe3dc929025da2003af0f6b916fa
jfigus <jfig_us@yahoo.com>
2013-11-06 14:12:12 -0500

    Fix libsrtp build.  Add GCM suites to SRTP transport layer.

--- pjproject-2.1.0/pjmedia/src/pjmedia/transport_srtp.c
+++ pjproject-2.1.0/pjmedia/src/pjmedia/transport_srtp.c
@@ -71,8 +71,32 @@ static crypto_suite crypto_suites[] = {
     /* plain RTP/RTCP (no cipher & no auth) */
     {"NULL", NULL_CIPHER, 0, NULL_AUTH, 0, 0, 0, sec_serv_none},
 
+    /* cipher AES_GCM, NULL auth, auth tag len = 8 octets */
+    {"AES_GCM_256_8", AES_256_GCM, 46, NULL_AUTH, 0, 8, 8, 
+	sec_serv_conf_and_auth},
+
+    /* cipher AES_GCM, NULL auth, auth tag len = 8 octets */
+    {"AES_GCM_128_8", AES_128_GCM, 30, NULL_AUTH, 0, 8, 8, 
+	sec_serv_conf_and_auth},
+
+     /* cipher AES_CM, auth HMAC_SHA1, auth tag len = 10 octets */
+    {"AES_CM_256_HMAC_SHA1_80", AES_256_ICM, 46, HMAC_SHA1, 20, 10, 10, 
+	sec_serv_conf_and_auth},
+
+    /* cipher AES_CM, auth HMAC_SHA1, auth tag len = 10 octets */
+    {"AES_CM_192_HMAC_SHA1_80", AES_192_ICM, 38, HMAC_SHA1, 20, 10, 10, 
+	sec_serv_conf_and_auth},
+
     /* cipher AES_CM, auth HMAC_SHA1, auth tag len = 10 octets */
     {"AES_CM_128_HMAC_SHA1_80", AES_128_ICM, 30, HMAC_SHA1, 20, 10, 10, 
+	sec_serv_conf_and_auth},
+
+     /* cipher AES_CM, auth HMAC_SHA1, auth tag len = 4 octets */
+    {"AES_CM_256_HMAC_SHA1_32", AES_256_ICM, 46, HMAC_SHA1, 20, 4, 10,
+	sec_serv_conf_and_auth},
+
+    /* cipher AES_CM, auth HMAC_SHA1, auth tag len = 4 octets */
+    {"AES_CM_192_HMAC_SHA1_32", AES_192_ICM, 38, HMAC_SHA1, 20, 4, 10,
 	sec_serv_conf_and_auth},
 
     /* cipher AES_CM, auth HMAC_SHA1, auth tag len = 4 octets */
@@ -315,7 +339,7 @@ static void pjmedia_srtp_deinit_lib(pjmedia_endpt *endpt)
 
     PJ_UNUSED_ARG(endpt);
 
-    err = srtp_deinit();
+    err = srtp_shutdown();
     if (err != err_status_ok) {
 	PJ_LOG(4, (THIS_FILE, "Failed to deinitialize libsrtp: %s", 
 		   get_libsrtp_errstr(err)));
---
+++ pjproject-2.1.0/third_party/build/srtp/config.h
@@ -0,0 +1,1 @@
+srtp_config.h
